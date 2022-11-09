f1ee4820ca6c10d2acc526991fa42cb6f0e89cad
ntpt7921 <83776509+ntpt7921@users.noreply.github.com>
2022-11-09 10:50:54 +0700

    Start to project, add (maybe) needed file

---
+++ Core/Inc/Custom/fsm_state_list.h
@@ -0,0 +1,25 @@
+/*
+ * fsm_state_list.h
+ *
+ *  Created on: Nov 1, 2022
+ *      Author: ntpt
+ */
+
+#ifndef INC_CUSTOM_FSM_STATE_LIST_H_
+#define INC_CUSTOM_FSM_STATE_LIST_H_
+
+typedef enum
+{
+	STATE_0,
+	STATE_1,
+	STATE_2,
+	STATE_3,
+	STATE_4,
+	STATE_5,
+	STATE_6,
+	STATE_7,
+	STATE_8,
+	STATE_9,
+} FSM_state_t;
+
+#endif /* INC_CUSTOM_FSM_STATE_LIST_H_ */
---
+++ Core/Inc/Custom/software_timer.h
@@ -0,0 +1,31 @@
+/*
+ * software_timer.h
+ *
+ *  Created on: Oct 24, 2022
+ *      Author: ntpt
+ */
+
+#ifndef INC_SOFTWARE_TIMER_H_
+#define INC_SOFTWARE_TIMER_H_
+
+#include "main.h"
+
+#define TIMER_TICK_DURATION_MS 10
+
+typedef enum
+{
+	TIMER_FLAG_RESET = RESET, TIMER_FLAG_SET = SET,
+} Timer_flag_state_t;
+
+
+typedef struct
+{
+	uint32_t timer_counter;
+	Timer_flag_state_t timer_flag;
+} Software_timer_t;
+
+void software_timer_set_duration_ms(volatile Software_timer_t *tm, uint32_t ms);
+void software_timer_update_after_tick(volatile Software_timer_t *tm);
+uint8_t software_timer_is_set(volatile Software_timer_t *tm);
+
+#endif /* INC_SOFTWARE_TIMER_H_ */
