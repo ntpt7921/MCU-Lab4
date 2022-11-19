9aea396f49e726516432d1efdb1afb0299b079e4
ntpt7921 <83776509+ntpt7921@users.noreply.github.com>
2022-11-19 09:45:34 +0700

    Adding new operations for priority queue
    Added function API to push down top element and delete element.
    
    The added delete can remove element from arbitrary position in the
    queue, compared to only the top element when popping.
    
    This is add to help delete task with specified ID when used with the
    scheduler.

--- Core/Inc/Custom/priority_queue.h
+++ Core/Inc/Custom/priority_queue.h
@@ -21,13 +21,15 @@ typedef uint8_t (*Compare_function_t)(void*, void*);
  * queue (binary heap). Whether it is max-heap or min-heap is not important,
  * since we will provide the comparison function between element.
  *
- * It is assumed that the array containing the heap already exists, and 
+ * It is assumed that the array containing the heap already exists, and
  * we can access the top element of the heap at array[0]
  *
  * The operations defined for the heap are:
  * - heap_create: make heap from random array
  * - heap_insert: insert a element into the heap
  * - heap_pop: remove the top element of the heap
+ * - heap_delete: remove the element at specified index
+ * - heap_push_down: try to push down the element at the top
  *
  * This module is written with reuse in mind, so it is necessarily general
  * and unoptimized. Each function takes a slew of argument:
@@ -44,6 +46,10 @@ void Custom_PQueue_Create(void *arr, size_t asize, size_t esize, size_t elemc,
 void Custom_PQueue_Insert(void *arr, size_t asize, size_t esize, size_t elemc, void *elem,
         Compare_function_t cmp);
 void Custom_PQueue_Pop(void *arr, size_t asize, size_t esize, size_t elemc,
+        Compare_function_t cmp);
+void Custom_PQueue_Delete(void *arr, size_t asize, size_t esize, size_t elemc, size_t index,
+        Compare_function_t cmp);
+void Custom_PQueue_PushDown(void *arr, size_t esize, size_t elemc,
         Compare_function_t cmp);
 
 #endif /* INC_CUSTOM_PRIORITY_QUEUE_H_ */
